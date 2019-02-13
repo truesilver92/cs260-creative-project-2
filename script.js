60a43e5a3a2f891f627ce5c2ad032738c7451e54
Joel Stoker <truesilver92@gmail.com>
2019-02-12 19:13:19 -0700

    refactored for legibility

--- script.js
+++ script.js
@@ -4,13 +4,12 @@ const search_box = document.getElementById("search-box");
 
 const keyup_f = () => {
     let search_text = search_box.value;
-    let debounce = _.debounce(
-        async () => {
-            console.log(baseurl + '&q=' + escape(search_text));
-            let results = await fetch(baseurl + '&q=' + escape(search_text));
-            console.log(results);
-            search_box.removeEventListener('keyup', debounce.cancel);
-        }, 300);
+    let api_search = async () => {
+        let results = await fetch(baseurl + '&q=' + escape(search_text));
+        console.log(results);
+        search_box.removeEventListener('keyup', debounce.cancel);
+    };
+    let debounce = _.debounce(api_search, 300);
     _.defer(debounce);
     search_box.addEventListener('keyup', debounce.cancel);
     return true;
