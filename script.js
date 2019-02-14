80f473fdb203e25f0e6d67d27811711f57609cfd
Joel Stoker <truesilver92@gmail.com>
2019-02-14 12:49:01 -0700

    centered all images and scaled down ones bigger than the screen

--- script.js
+++ script.js
@@ -4,6 +4,11 @@ const search_box = document.getElementById("search-box");
 var image_index = 0;
 var imgs = [];
 
+const on_window_resize = () => {
+    setBackgroundImage(imgs[image_index]);
+};
+window.addEventListener('resize', on_window_resize);
+
 // Search box event listener
 const keyup_f = () => {
 
@@ -42,6 +47,33 @@ const setBackgroundImage = async (jsonImage) => {
     let newImage = document.createElement('img');
     newImage.src = jsonImage.largeImageURL;
     newImage.id = 'mainImage';
+
+    let clientWidth = document.documentElement.clientWidth;
+    let clientHeight = document.documentElement.clientHeight;
+    let width = jsonImage.imageWidth;
+    let height = jsonImage.imageHeight;
+
+    // scale images down/up keeping aspect ratio
+    const width_match_screen = () => {
+        let factor = clientWidth / width;
+        width = clientWidth;
+        height = factor * height;
+    };
+    const height_match_screen = () => {
+        let factor = clientHeight / height;
+        height = clientHeight;
+        width = factor * width;
+    };
+    width_match_screen();
+    height_match_screen();
+    if(width < clientWidth)
+        width_match_screen();
+    newImage.style.width = width.toString() + 'px';
+    newImage.style.height = height.toString() + 'px';
+
+    // move image so image center = screen center
+    newImage.style.marginLeft = (width / -2).toString() + 'px';
+    newImage.style.marginTop = (height / -2).toString() + 'px';
 
     newImage.onload = () => {
         let loaded_time = Date.now();
